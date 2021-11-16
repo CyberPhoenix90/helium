c0c2d1d840f36a8cd2a9d2f74d8ec36166857d99
CyberPhoenix90 <Joao260690@gmail.com>
2021-11-16 22:38:46 +0100

    add programatic api

---
+++ compiler/src/cli.ts
@@ -0,0 +1,21 @@
+#!/bin/node
+
+import { existsSync } from 'fs';
+import { join } from 'path';
+import { parseArguments } from './args/arg_parser';
+import { Compiler } from './compiling/compiler';
+import { processCompilerConfig } from './config/config';
+
+const args = parseArguments(process.argv.slice(2));
+
+if (!args.map.p && !args.map.project) {
+	const path = join(process.cwd(), 'heconfig.json');
+	if (existsSync(path)) {
+		args.map.p = 'heconfig.json';
+	}
+}
+
+const compilerConfig = processCompilerConfig(args);
+const compiler = new Compiler(process.cwd(), compilerConfig);
+compiler.validate();
+compiler.emit();
--- compiler/src/config/config.ts
+++ compiler/src/config/config.ts
@@ -1,76 +1,91 @@
-import { existsSync } from 'fs';
-import { resolve } from 'path';
-import { ParsedArguments } from '../args/arg_parser';
+import { existsSync } from "fs";
+import { resolve } from "path";
+import { ParsedArguments } from "../args/arg_parser";
 
 export interface CompilerConfig {
-	version: string;
-	clientOut: TargetOutput[];
-	serverOut: TargetOutput[];
-	include: string[];
-	links?: Record<string, string>;
+    version: string;
+    clientOut: TargetOutput[];
+    serverOut: TargetOutput[];
+    include: string[];
+    links?: Record<string, string>;
 }
 
 export interface TargetOutput {
-	namespace: string;
-	path: string;
-	platform: 'browser' | 'electron' | 'nodejs' | 'dotnet';
+    namespace: string;
+    path: string;
+    platform:
+        | "browser"
+        | "electron"
+        | "nodejs"
+        | "dotnet"
+        | "jvm"
+        | "javascript";
 }
 
 export interface Output {
-	filePath: string;
-	fileContent: string;
+    filePath: string;
+    fileContent: string;
 }
 
 export function processCompilerConfig(args: ParsedArguments): CompilerConfig {
-	const configFileName = args.map.p ?? args.map.project;
-	if (!configFileName) {
-		throw new Error('No project file specified');
-	}
+    const configFileName = args.map.p ?? args.map.project;
+    if (!configFileName) {
+        throw new Error("No project file specified");
+    }
 
-	const configFilePath = resolve(process.cwd(), configFileName.toString());
+    const configFilePath = resolve(process.cwd(), configFileName.toString());
 
-	if (!existsSync(configFilePath)) {
-		throw new Error(`Project file ${configFilePath} does not exist`);
-	}
+    if (!existsSync(configFilePath)) {
+        throw new Error(`Project file ${configFilePath} does not exist`);
+    }
 
-	const config: CompilerConfig = require(configFilePath);
+    const config: CompilerConfig = require(configFilePath);
 
-	if (!config.clientOut?.length && !config.serverOut?.length) {
-		throw new Error('No output specified');
-	}
+    if (!config.clientOut?.length && !config.serverOut?.length) {
+        throw new Error("No output specified");
+    }
 
-	for (const clientOutput of config.clientOut ?? []) {
-		validateOutputGeneric(clientOutput);
-	}
+    for (const clientOutput of config.clientOut ?? []) {
+        validateOutputGeneric(clientOutput);
+    }
 
-	for (const serverOutput of config.serverOut ?? []) {
-		validateOutputGeneric(serverOutput);
-		if (serverOutput.platform === 'browser') {
-			throw new Error('Browser platform is not supported for server');
-		}
-	}
+    for (const serverOutput of config.serverOut ?? []) {
+        validateOutputGeneric(serverOutput);
+        if (serverOutput.platform === "browser") {
+            throw new Error("Browser platform is not supported for server");
+        }
+    }
 
-	if (config.include.length === 0) {
-		throw new Error('No source include specified');
-	}
+    if (config.include.length === 0) {
+        throw new Error("No source include specified");
+    }
 
-	return config;
+    return config;
 }
 
 function validateOutputGeneric(output: TargetOutput) {
-	if (!output.namespace) {
-		throw new Error('No namespace specified for output');
-	}
+    if (!output.namespace) {
+        throw new Error("No namespace specified for output");
+    }
 
-	if (!output.path) {
-		throw new Error('No path specified for  output');
-	}
+    if (!output.path) {
+        throw new Error("No path specified for  output");
+    }
 
-	if (!output.platform) {
-		throw new Error('No platform specified for output');
-	}
+    if (!output.platform) {
+        throw new Error("No platform specified for output");
+    }
 
-	if (!['browser', 'nodejs', 'dotnet', 'javascript', 'electron'].includes(output.platform)) {
-		throw new Error(`Unknown platform ${output.platform} for output`);
-	}
+    if (
+        ![
+            "browser",
+            "nodejs",
+            "dotnet",
+            "javascript",
+            "electron",
+            "jvm",
+        ].includes(output.platform)
+    ) {
+        throw new Error(`Unknown platform ${output.platform} for output`);
+    }
 }
